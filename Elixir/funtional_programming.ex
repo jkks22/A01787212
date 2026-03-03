968ee7ee162e393af6826cce91bf86cdcbfcac28
jkks22 <josuul21@gmail.com>
2026-03-03 10:08:45 -0600

    update

--- Elixir/funtional_programming.ex
+++ Elixir/funtional_programming.ex
@@ -1,6 +1,7 @@
 #Josue Gomez Artaga A01787212
 
 defmodule Hw.Ariel1 do
+
   def fahrenheit_to_celsius(fahrenheit) do
     5 * (fahrenheit - 32) / 9
   end
@@ -32,7 +33,7 @@ defmodule Hw.Ariel1 do
   def factorial(n), do: n * factorial(n - 1)
 
   def pow(_a, 0), do: 1
-  def pow(a, b),  do: a * pow(a, b - 1)
+  def pow(a, b), do: a * pow(a, b - 1)
 
   def fib(0), do: 0
   def fib(1), do: 1
@@ -41,29 +42,29 @@ defmodule Hw.Ariel1 do
   def duplicate([]), do: []
   def duplicate([head | tail]), do: [head, head | duplicate(tail)]
 
-  def enlist([]),            do: []
+  def enlist([]), do: []
   def enlist([head | tail]), do: [[head] | enlist(tail)]
 
-  def positives([]),            do: []
+  def positives([]), do: []
   def positives([head | tail]) when head > 0, do: [head | positives(tail)]
-  def positives([_head | tail]),              do: positives(tail)
+  def positives([_head | tail]), do: positives(tail)
 
-  def add_list([]),            do: 0
+  def add_list([]), do: 0
   def add_list([head | tail]), do: head + add_list(tail)
 
-  def invert_pairs([]),              do: []
+  def invert_pairs([]), do: []
   def invert_pairs([{a, b} | tail]), do: [{b, a} | invert_pairs(tail)]
 
-  def is_atom_list([]),            do: true
+  def is_atom_list([]), do: true
   def is_atom_list([head | tail]) when is_atom(head), do: is_atom_list(tail)
-  def is_atom_list([_head | _tail]),                  do: false
+  def is_atom_list([_head | _tail]), do: false
 
-  def swapper([], _a, _b),          do: []
-  def swapper([a | tail], a, b),    do: [b | swapper(tail, a, b)]
-  def swapper([b | tail], a, b),    do: [a | swapper(tail, a, b)]
+  def swapper([], _a, _b), do: []
+  def swapper([a | tail], a, b), do: [b | swapper(tail, a, b)]
+  def swapper([b | tail], a, b), do: [a | swapper(tail, a, b)]
   def swapper([head | tail], a, b), do: [head | swapper(tail, a, b)]
 
-  def dot_product([], []),               do: 0
+  def dot_product([], []), do: 0
   def dot_product([ha | ta], [hb | tb]), do: ha * hb + dot_product(ta, tb)
 
   def average([]), do: 0
@@ -87,7 +88,7 @@ defmodule Hw.Ariel1 do
   def repetir_elemento(_elemento, 0), do: []
   def repetir_elemento(elemento, n),  do: [elemento | repetir_elemento(elemento, n - 1)]
 
-  def replic(_n, []),    do: []
+  def replic(_n, []), do: []
   def replic(0, _lista), do: []
   def replic(n, [head | tail]) do
     repetir_elemento(head, n) ++ replic(n, tail)
